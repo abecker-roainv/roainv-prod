43847cb5a5cf98fc2730d7f7f7afbf3df7b8f55f
Andrés Becker <andres@smart-broker.pro>
2025-01-15 12:34:56 -0300

    Update page.js

--- app/(protected)/popestate/page.js
+++ app/(protected)/popestate/page.js
@@ -1,12 +1,12 @@
 "use client";
 
 import { Box } from '@mui/material';
-import UnitsTable from '@/components/UnitsTable';
+// import UnitsTable from '@/components/UnitsTable';
 
 export default function UnitsPage() {
   return (
     <Box sx={{ p: 3 }}>
-      <UnitsTable />
+      {/* <UnitsTable /> */}
     </Box>
   );
 }
